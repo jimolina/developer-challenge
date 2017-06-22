4e707839e5cadef0d815482095eeb0af4eb6dd5c
MarcHines <mxh857@miami.edu>
2017-06-22 11:06:53 -0400

    Laying the foundation for the midsection of the page. Next step is to fix margin and padding to reflect previous sections. I want to clean up some of the css. I want to minimize the amount of classes and id's

--- header.php
+++ header.php
@@ -1,4 +1,16 @@
-<header>
-    <h1><strong>We've created product that will help your startup to look even better</strong></h1>
-    <h3> You have the design, you have the code </h3>
-</header>
+<div class="jumbotron">
+   <div class="container-fluid">
+       <div class="row">
+           <div class="col-md-8 featured-text">
+            <p id="header-title"> We've created the product that will help your startup look better</p>
+            
+            <h2 class="text-white"> You have the design, you have the code</h2>
+           </div>
+           
+           <div class="col-md-4">
+               
+           </div>  
+       </div>
+      
+    </div>
+  </div>
