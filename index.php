26b21248d107f7291e765c7b183686b304499343
MarcHines <mxh857@miami.edu>
2017-06-22 00:57:03 -0400

    Initial Commit. The plan is to break the page into smaller, more manageble ections

---
+++ index.php
@@ -0,0 +1,59 @@
+
+<!DOCTYPE html>
+<html>
+    <script
+  src="https://code.jquery.com/jquery-3.2.1.min.js"
+  integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+  crossorigin="anonymous">
+  </script>
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+ 
+<head>
+      <meta charset="UTF-8">
+      <title>Challenge</title>
+      <link href="styles.css" type="text/css" rel="stylesheet" /> 
+</head>    
+    <body>
+    <nav class="navbar navbar-default nav-bar-custom">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span> 
+      </button>
+      <a class="navbar-brand" href="#"></a>
+    </div>
+      
+    <div class="collapse navbar-collapse">
+      <ul class="nav navbar-nav navbar-right nav-list-items">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Work</a></li>
+            <li><a href="#">Blog</a></li>
+            <li id="last-list-item"><a href="#">Contact</a></li>
+         
+          <button class="btn btn-info navbar-btn" id="btn-log-in">Sign In</button>
+      </ul>
+
+    </div> <!-- End of Navbar Collapse -->
+  </div>
+</nav>               
+        <div class="jumbotron" id="jumbo">
+           
+        </div>
+        <div class="container-fluid">
+          
+            
+            
+        </div>
+        
+    </body>
+</html>
