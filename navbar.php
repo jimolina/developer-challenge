a0a1530f569b5c630ce542ce8753fcf4243b261c
MarcHines <mxh857@miami.edu>
2017-06-22 02:07:06 -0400

    Basic navigation bar. I started to break up the file into sections, and use php to include.Keeps our index file cleaner with less code to decipher later if issues arise

---
+++ navbar.php
@@ -0,0 +1,29 @@
+  <nav class="navbar navbar-default nav-bar-custom">
+     <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span> 
+      </button>
+      <a class="navbar-brand" href="#"></a>
+    </div>
+      <ul class="nav navbar-nav">
+          <a href="#" class="navbar-left"><img src="./resources/logo-icon.png"></a>
+          <p class="navbar-text">Startup</p> 
+          
+      </ul>
+         
+    <div class="collapse navbar-collapse">
+      <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Work</a></li>
+            <li><a href="#">Blog</a></li>
+            <li id="last-list-item"><a href="#">Contact</a></li>
+         
+          <button class="btn btn-info navbar-btn" id="btn-log-in">Sign In</button>
+      </ul>
+
+    </div> <!-- End of Navbar Collapse -->
+  </div>
+</nav>  
